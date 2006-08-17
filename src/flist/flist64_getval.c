c8daccbaa44631cf4c265dc2121962e1e60e0b49
Benedikt Böhm <bb@xnull.de>
2006-08-17 08:03:15 +0000

    header fixups

--- src/flist/flist64_getval.c
+++ src/flist/flist64_getval.c
@@ -15,7 +15,7 @@
 // Free Software Foundation, Inc.,
 // 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
-#include <strings.h>
+#include <string.h>
 #include <errno.h>
 
 #include "flist.h"
