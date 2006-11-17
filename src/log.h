1d5925acac954edbac1427979ef4fe11de3e384b
Benedikt Böhm <bb@xnull.de>
2006-11-17 23:50:51 +0000

    add inline doc

--- src/log.h
+++ src/log.h
@@ -26,6 +26,7 @@
 #include <stdarg.h>
 #include <libgen.h>
 
+/*! @brief log system configuration data */
 typedef struct {
 	char *ident;
 	bool  file;
@@ -38,41 +39,249 @@ typedef struct {
 	int   mask;
 } log_options_t;
 
+/*!
+ * @brief open connection to logging system
+ *
+ * @param options log system configuration options
+ */
 void log_init(log_options_t *options);
 
 void log_internal(int level, int strerr, const char *fmt, va_list ap);
 
+/*! @brief simple trace helper */
 #define LOG_TRACEME log_debug("[trace] %s (%s:%d)", \
                           __FUNCTION__, basename(__FILE__), __LINE__);
 
-void log_emerg (const char *fmt, ...);
-void log_alert (const char *fmt, ...);
-void log_crit  (const char *fmt, ...);
-void log_error (const char *fmt, ...);
-void log_warn  (const char *fmt, ...);
-void log_notice(const char *fmt, ...);
-void log_info  (const char *fmt, ...);
-void log_debug (const char *fmt, ...);
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_emerg(const char *fmt, ...);
 
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_alert(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_crit(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_error(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_warn(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_notice(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_info(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_debug(const char *fmt, ...);
+
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
 void log_emerg_and_die(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
 void log_alert_and_die(const char *fmt, ...);
-void log_crit_and_die (const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
+void log_crit_and_die(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
 void log_error_and_die(const char *fmt, ...);
 
-void log_pemerg (const char *fmt, ...);
-void log_palert (const char *fmt, ...);
-void log_pcrit  (const char *fmt, ...);
-void log_perror (const char *fmt, ...);
-void log_pwarn  (const char *fmt, ...);
-void log_pnotice(const char *fmt, ...);
-void log_pinfo  (const char *fmt, ...);
-void log_pdebug (const char *fmt, ...);
 
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_pemerg(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_palert(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_pcrit(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_FAILURE
+ */
+int log_perror(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_pwarn(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_pnotice(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_pinfo(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ *
+ * @return EXIT_SUCCESS
+ */
+int log_pdebug(const char *fmt, ...);
+
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
 void log_pemerg_and_die(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
 void log_palert_and_die(const char *fmt, ...);
-void log_pcrit_and_die (const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
+void log_pcrit_and_die(const char *fmt, ...);
+
+/*!
+ * @brief write message to logging system
+ *
+ * @param fmt format string passed to printf(3)
+ * @param ... variable number of arguments according to fmt
+ */
 void log_perror_and_die(const char *fmt, ...);
 
+/*!
+ * @brief close connection to logging system
+ */
 void log_close(void);
 
 #define _LUCID_LOG_H
