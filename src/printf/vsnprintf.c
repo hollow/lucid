5c6fb72c62164934862497c5008e29852ca78bb4
Benedikt Böhm <bb@xnull.de>
2006-11-18 17:59:17 +0000

    cleanup printf: fix certain corner cases, add %n conversion

--- src/printf/vsnprintf.c
+++ src/printf/vsnprintf.c
@@ -16,7 +16,6 @@
 // 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <stdlib.h>
-#include <string.h>
 
 #include "fmt.h"
 #include "printf.h"
@@ -33,181 +32,136 @@ typedef struct {
 
 typedef struct {
 	int isset;
-	unsigned int width;
+	int width;
 } __prec_t;
 
 typedef struct {
 	__flags_t f;
 	__prec_t  p;
-	unsigned int w;
-	unsigned int l;
-	unsigned int c;
+	int w;
+	int l;
 } __printf_t;
 
 /* supported formats:
 ** - format flags: #, 0, -, ' ', +
 ** - field width
 ** - argument precision
-** - length mods: hh, h, l, z (handled as long); ll (handled as long long)
+** - length mods: hh, h, l, (handled as long); ll (handled as long long)
 ** - conversion spec: d, i, u, o, x, f, c, s, p */
-int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
+int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list _ap)
 {
 	/* keep track of string length */
 	size_t idx = 0;
 	
-	while (*fmt) {
-		if (*fmt != '%') {
+	int l, i;
+	char c;
+	const char *fmtp = fmt;
+	
+	va_list ap = va_copy(_ap);
+	
+	/* sanitize destination buffer */
+	for (i = 0; i < size; i++)
+		*(str+i) = 0;
+	
+	while ((c = *fmt++)) {
+		if (c != '%') {
 			if (idx < size)
-				str[idx++] = *fmt++;
-			else {
-				idx++;
-				fmt++;
-			}
+				str[idx] = c;
+			
+			idx++;
 		}
 		
 		else {
-			/* skip % */
-			fmt++;
-			
 			/* format struct */
 			__printf_t f;
 			
 			/* sanitize struct with default values */
+			f.w = 0;
+			f.l = 0;
+			
 			f.f.alt   = 0;
 			f.f.zero  = 0;
 			f.f.left  = 0;
 			f.f.blank = 0;
 			f.f.sign  = 0;
 			
-			f.w = 0;
-			
 			f.p.isset = 0;
 			f.p.width = 1;
 			
-			f.l = '\0';
-			f.c = '\0';
 			
 			/* parse flags */
-			while (*fmt == '0' || *fmt == '-' || *fmt == '+' || *fmt == ' ' || *fmt == '#') {
-				switch (*fmt) {
-					case '#': f.f.alt   = 1; break;
-					case '0': f.f.zero  = 1; break;
-					case '-': f.f.left  = 1; break;
-					case ' ': f.f.blank = 1; break;
-					case '+': f.f.sign  = 1; break;
-				}
-				
-				fmt++;
+			while ((c = *fmt++)) {
+				if      (c == '#') f.f.alt   = 1;
+				else if (c == '0') f.f.zero  = 1;
+				else if (c == '-') f.f.left  = 1;
+				else if (c == ' ') f.f.blank = 1;
+				else if (c == '+') f.f.sign  = 1;
+				else               break;
 			}
 			
 			/* parse field width */
-			if (*fmt == '*') {
-				fmt++;
+			if (c == '*') {
+				l = va_arg(ap, int);
 				
-				int arg = va_arg(ap, int);
-				
-				if (arg >= 0)
-					f.w = arg;
+				if (l >= 0)
+					f.w = l;
 				else {
-					f.w = -arg;
+					f.w = -l;
 					f.f.left = 1;
 				}
-			} else if (char_isdigit(*fmt)) {
-				f.w = *fmt - '0';
 				
-				fmt ++;
-				
-				while (char_isdigit(*fmt)) {
-					f.w = 10 * f.w + (unsigned int)(*fmt - '0');
-					fmt++;
-				}
+				c = *fmt++;
+			}
+			
+			else if (char_isdigit(c)) {
+				do {
+					f.w = f.w * 10 + c - '0';
+					c = *fmt++;
+				} while (char_isdigit(c));
 			}
 			
 			/* parse precision */
-			if (*fmt == '.') {
-				fmt++;
+			if (c == '.') {
+				c = *fmt++;
 				
 				f.p.isset = 1;
 				f.p.width = 0;
 				
-				if (*fmt == '*') {
-					fmt++;
+				if (c == '*') {
+					l = va_arg(ap, int);
 					
-					int arg = va_arg(ap, int);
+					if (l >= 0)
+						f.p.width = l;
 					
-					if (arg >= 0)
-						f.p.width = arg;
-				} else if (char_isdigit(*fmt)) {
-					f.p.width = *fmt - '0';
-					
-					fmt++;
-					
-					while (char_isdigit(*fmt)) {
-						f.p.width = 10 * f.p.width + (unsigned int)(*fmt - '0');
-						fmt++;
-					}
+					c = *fmt++;
+				}
+				
+				else if (char_isdigit(c)) {
+					do {
+						f.p.width = f.p.width * 10 + c - '0';
+						c = *fmt++;
+					} while (char_isdigit(c));
 				}
 			}
 			
 			/* parse length modifier
-			** hh, h and l, z are all handled as long int
-			** ll, L is handled as long long int */
-			switch (*fmt) {
-				case 'h':
-				case 'z':
-					f.l = 'l';
-					
-					if (*(fmt+1) == 'h')
-						fmt++;
-					
-					break;
+			** hh, h and l, are all handled as long int
+			** ll is handled as long long int */
+			if (c == 'l') {
+				c = *fmt++;
 				
-				case 'l':
-					f.l = 'l';
-					
-					if (*(fmt+1) == 'l') {
-						f.l = '2';
-						fmt++;
-					}
-					
-					break;
-				
-				case 'L':
-					f.l = '2';
-					break;
-				
-				default: break;
+				if (c == 'l') {
+					f.l = 1;
+					c = *fmt++;
+				}
 			}
 			
-			if (f.l != '\0')
-				fmt++;
-			
-			/* parse conversion specifier */
-			switch (*fmt) {
-				case 'd':
-				case 'i':
-					f.c = 'd';
-					break;
+			else if (c == 'h') {
+				c = *fmt++;
 				
-				case 'f':
-				
-				case 'o':
-				case 'u':
-				case 'x':
-				
-				case 'c':
-				case 's':
-				
-				case 'p':
-				
-				case '%':
-					f.c = *fmt;
-					break;
-				
-				default: break;
+				if (c == 'h')
+					c = *fmt++;
 			}
-			
-			fmt++;
 			
 			/* sign overrides blank */
 			if (f.f.sign)
@@ -221,9 +175,9 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 			if (f.p.isset && f.w > 0)
 				f.f.zero = 0;
 			
-			/* no zero padding for string conversions */
-			if (f.c == 'c' || f.c == 's')
-				f.f.zero = 0;
+			/* conversion functions */
+			size_t (*ufn)  (char *, unsigned long);
+			size_t (*lufn) (char *, unsigned long long);
 			
 			/* signed long argument */
 			signed long darg; signed long long ldarg;
@@ -238,10 +192,13 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 			int carg;
 			
 			/* string argument */
-			char *sarg;
+			const char *sarg;
 			
 			/* pointer argument */
 			void *parg;
+			
+			/* number argument */
+			int *narg;
 			
 			/* used for precision counting */
 			int len;
@@ -250,28 +207,28 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 			char ibuf[64];
 			
 			/* temporary buffer for string conversion */
-			char *buf = ibuf;
+			const char *buf = ibuf;
 			
 			/* buffer length */
 			size_t buflen = 0;
 			
 			/* do conversions */
-			switch (f.c) {
-				case 'd': /* signed conversion */
-					if (f.l == '2') { /* long long */
+			switch (c) {
+				case 'd':
+				case 'i': /* signed conversion */
+					if (f.l) { /* long long */
 						ldarg = va_arg(ap, signed long long);
 						
 						if (ldarg == 0 && f.p.isset && f.p.width == 0)
 							break;
 						
 						/* forced sign */
-						if (f.f.sign) {
+						if (f.f.sign)
 							buflen += fmt_plusminus(&ibuf[buflen], ldarg);
-						} else {
+						else
 							buflen += fmt_minus(&ibuf[buflen], ldarg);
-						}
 						
-						len = fmt_ulonglong(FMT_LEN, (unsigned long long) abs(ldarg));
+						len = fmt_ulonglong(FMT_LEN, (unsigned long long) llabs(ldarg));
 						
 						while ((unsigned long) len < f.p.width) {
 							buflen += fmt_str(&ibuf[buflen], "0");
@@ -279,18 +236,19 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 						}
 					
 						buflen += fmt_ulonglong(&ibuf[buflen], (unsigned long long) llabs(ldarg));
-					} else { /* !long long */
+					}
+					
+					else { /* !long long */
 						darg = va_arg(ap, signed long);
 						
 						if (darg == 0 && f.p.isset && f.p.width == 0)
 							break;
 						
 						/* forced sign */
-						if (f.f.sign) {
+						if (f.f.sign)
 							buflen += fmt_plusminus(&ibuf[buflen], darg);
-						} else {
+						else
 							buflen += fmt_minus(&ibuf[buflen], darg);
-						}
 						
 						len = fmt_ulong(FMT_LEN, (unsigned long) labs(darg));
 						
@@ -301,6 +259,66 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 					
 						buflen += fmt_ulong(&ibuf[buflen], (unsigned long) labs(darg));
 					}
+					
+					break;
+				
+				/* unsigned conversions */
+				case 'o': /* octal notation */
+				case 'u': /* decimal notation */
+				case 'x': /* hexadecimal notation */
+					if (c == 'o') {
+						ufn = fmt_8long;
+						lufn = fmt_8longlong;
+					}
+					
+					else if (c == 'u') {
+						ufn = fmt_ulong;
+						lufn = fmt_ulonglong;
+					}
+					
+					else {
+						ufn = fmt_xlong;
+						lufn = fmt_xlonglong;
+					}
+					
+					if (f.l) { /* long long */
+						luarg = va_arg(ap, unsigned long long);
+						
+						if (luarg == 0 && f.p.isset && f.p.width == 0)
+							break;
+						
+						if (f.f.alt)
+							buflen += fmt_str(&ibuf[buflen], "0");
+						
+						len = lufn(FMT_LEN, luarg);
+						
+						while ((unsigned long) len < f.p.width) {
+							buflen += fmt_str(&ibuf[buflen], "0");
+							f.p.width--;
+						}
+						
+						buflen += lufn(&ibuf[buflen], luarg);
+					}
+					
+					else { /* !long long */
+						uarg = va_arg(ap, unsigned long);
+						
+						if (uarg == 0 && f.p.isset && f.p.width == 0)
+							break;
+						
+						if (f.f.alt)
+							buflen += fmt_str(&ibuf[buflen], "0");
+						
+						len = ufn(FMT_LEN, uarg);
+						
+						while ((unsigned long) len < f.p.width) {
+							buflen += fmt_str(&ibuf[buflen], "0");
+							f.p.width--;
+						}
+						
+						buflen += ufn(&ibuf[buflen], uarg);
+					}
+					
 					break;
 				
 				case 'f': /* float conversion */
@@ -310,137 +328,38 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 						f.p.width = 6;
 					
 					/* forced sign (fmt_double adds minus) */
-					if (f.f.sign && farg >= 0) {
+					if (f.f.sign && farg >= 0)
 						buflen += fmt_str(&ibuf[buflen], "+");
-					}
 					
 					buflen += fmt_double(&ibuf[buflen], farg, f.p.width);
 					break;
 				
-				/* unsigned conversions */
-				case 'o': /* octal representation */
-					if (f.l == '2') { /* long long */
-						luarg = va_arg(ap, unsigned long long);
-						
-						if (luarg == 0 && f.p.isset && f.p.width == 0)
-							break;
-						
-						if (f.f.alt)
-							buflen += fmt_str(&ibuf[buflen], "0");
-						
-						len = fmt_8long(FMT_LEN, luarg);
-						
-						while ((unsigned long) len < f.p.width) {
-							buflen += fmt_str(&ibuf[buflen], "0");
-							f.p.width--;
-						}
-						
-						buflen += fmt_8longlong(&ibuf[buflen], luarg);
-					} else { /* !long long */
-						uarg = va_arg(ap, unsigned long);
-						
-						if (uarg == 0 && f.p.isset && f.p.width == 0)
-							break;
-						
-						if (f.f.alt)
-							buflen += fmt_str(&ibuf[buflen], "0");
-						
-						len = fmt_8longlong(FMT_LEN, uarg);
-						
-						while ((unsigned long) len < f.p.width) {
-							buflen += fmt_str(&ibuf[buflen], "0");
-							f.p.width--;
-						}
-						
-						buflen += fmt_8long(&ibuf[buflen], uarg);
-					}
-					
-					break;
-				
-				case 'u': /* decimal representation */
-					if (f.l == '2') {
-						luarg = va_arg(ap, unsigned long long);
-						
-						if (luarg == 0 && f.p.isset && f.p.width == 0)
-							break;
-						
-						len = fmt_ulonglong(FMT_LEN, luarg);
-						
-						while ((unsigned long) len < f.p.width) {
-							buflen += fmt_str(&ibuf[buflen], "0");
-							f.p.width--;
-						}
-					
-						buflen += fmt_ulonglong(&ibuf[buflen], luarg);
-					} else {
-						uarg = va_arg(ap, unsigned long);
-						
-						if (uarg == 0 && f.p.isset && f.p.width == 0)
-							break;
-						
-						len = fmt_ulong(FMT_LEN, uarg);
-						
-						while ((unsigned long) len < f.p.width) {
-							buflen += fmt_str(&ibuf[buflen], "0");
-							f.p.width--;
-						}
-					
-						buflen += fmt_ulong(&ibuf[buflen], uarg);
-					}
-					break;
-				
-				case 'x': /* hex conversion */
-					if (f.l == '2') {
-						luarg = va_arg(ap, unsigned long long);
-						
-						if (luarg == 0 && f.p.isset && f.p.width == 0)
-							break;
-						
-						if (f.f.alt)
-							buflen += fmt_str(&ibuf[buflen], "0x");
-						
-						len = fmt_xlonglong(FMT_LEN, luarg);
-						
-						while ((unsigned long) len < f.p.width) {
-							buflen += fmt_str(&ibuf[buflen], "0");
-							f.p.width--;
-						}
-						
-						buflen += fmt_xlonglong(&ibuf[buflen], luarg);
-					} else {
-						uarg = va_arg(ap, unsigned long);
-						
-						if (uarg == 0 && f.p.isset && f.p.width == 0)
-							break;
-						
-						if (f.f.alt)
-							buflen += fmt_str(&ibuf[buflen], "0x");
-						
-						len = fmt_xlong(FMT_LEN, uarg);
-						
-						while ((unsigned long) len < f.p.width) {
-							buflen += fmt_str(&ibuf[buflen], "0");
-							f.p.width--;
-						}
-						
-						buflen += fmt_xlong(&ibuf[buflen], uarg);
-					}
-					break;
-				
 				case 'c': /* character conversion */
+					/* no zero padding for string conversions */
+					f.f.zero = 0;
+					
 					carg = va_arg(ap, int);
-					ibuf[buflen++] = (char) carg;
+					ibuf[buflen++] = (unsigned char) carg;
 					break;
 				
 				case 's': /* string conversion */
-					sarg = va_arg(ap, char *);
+					/* no zero padding for string conversions */
+					f.f.zero = 0;
+					
+					sarg = va_arg(ap, const char *);
 					buf = sarg;
 					
-					if (!buf) buflen = 0;
-					else if (!f.p.isset) buflen = strlen(buf);
-					else if (f.p.width == 0) buflen = 0;
+					if (!buf)
+						buflen = 0;
+					
+					else if (!f.p.isset)
+						buflen = str_len(buf);
+					
+					else if (f.p.width == 0)
+						buflen = 0;
+					
 					else {
-						const char *q = memchr(buf, '\0', f.p.width);
+						const char *q = str_index(buf, '\0', f.p.width);
 						
 						if (q == 0) /* string will be truncated */
 							buflen = f.p.width;
@@ -459,7 +378,34 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 					buflen += fmt_xlong(&ibuf[buflen], (unsigned long) parg);
 					break;
 				
-				default: break;
+				case 'n':
+					narg  = va_arg(ap, int *);
+					*narg = idx;
+					
+					break;
+				
+				case '%':
+					/* no padding for % */
+					f.w = 0;
+					
+					ibuf[buflen++] = '%';
+				
+				default:
+					/* no padding for unknown conversion */
+					f.w = 0;
+					
+					buf    = fmtp;
+					buflen = str_len(fmtp);
+					fmt    = fmtp + buflen;
+					
+					const char *q = str_index(buf + 1, '%', buflen - 1);
+					
+					if (q != 0) {
+						buflen = q - buf - 1;
+						fmt    = q - 1;
+					}
+					
+					break;
 			}
 			
 			/* blank/zero padding using right alignment */
@@ -470,14 +416,14 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 							fmt_strn(&str[idx++], "0", 1);
 						else
 							fmt_strn(&str[idx++], " ", 1);
-					} else
+					}
+					
+					else
 						idx++;
 					
 					f.w--;
 				}
 			}
-			
-			size_t i;
 			
 			/* write the converted argument */
 			for (i = 0; i < buflen; i++) {
@@ -495,17 +441,20 @@ int _lucid_vsnprintf(char *str, size_t size, const char *fmt, va_list ap)
 							fmt_strn(&str[idx++], "0", 1);
 						else
 							fmt_strn(&str[idx++], " ", 1);
-					} else
+					}
+					
+					else
 						idx++;
 					
 					f.w--;
 				}
 			}
+			
+			fmtp = &c;
 		}
 	}
 	
-	if (idx < size)
-		str[idx] = '\0';
+	va_end(ap);
 	
 	return idx;
 }
