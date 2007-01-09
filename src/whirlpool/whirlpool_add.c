2e08969984d3f6fd0445f431517d5cf21b62942e
Benedikt Böhm <bb@xnull.de>
2007-01-09 16:14:02 +0000

    license header fixage

--- src/whirlpool/whirlpool_add.c
+++ src/whirlpool/whirlpool_add.c
@@ -1,13 +1,13 @@
-// Copyright 2006 Benedikt Böhm <hollow@gentoo.org>
+// Copyright (C) 2006-2007 Benedikt Böhm <hollow@gentoo.org>
 //
 // The Whirlpool algorithm was developed by
 //                Paulo S. L. M. Barreto <pbarreto@scopus.com.br> and
 //                Vincent Rijmen <vincent.rijmen@cryptomathic.com>
 //
-// This program is free software; you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation; either version 2 of the License, or
-// (at your option) any later version.
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
 // This program is distributed in the hope that it will be useful,
 // but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -15,9 +15,8 @@
 // GNU General Public License for more details.
 //
 // You should have received a copy of the GNU General Public License
-// along with this program; if not, write to the
-// Free Software Foundation, Inc.,
-// 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
 #include "whirlpool.h"
 
