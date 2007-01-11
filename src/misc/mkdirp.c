79254c7efa5ef1e3180e4be0be4a304eecf6159f
Benedikt Böhm <bb@xnull.de>
2007-01-11 19:21:08 +0000

    use strtok for path walk

--- src/misc/mkdirp.c
+++ src/misc/mkdirp.c
@@ -14,67 +14,60 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
+#include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
 #include <sys/stat.h>
 
 #include "misc.h"
+#include "open.h"
 #include "str.h"
+#include "strtok.h"
 
 int mkdirp(const char *path, mode_t mode)
 {
-	int errno_orig, rc = 0;
-	char *p, *buf, c;
+	int ok = 1;
 	struct stat sb;
 	
-	if (str_isempty(path))
+	if (str_isempty(path) || str_path_isdot(path))
 		return errno = EINVAL, -1;
 	
-	buf = p = str_dup(path);
+	strtok_t _st, *st = &_st, *p;
 	
-	do {
-		c = 0;
-		
-		while (*p) {
-			if (*p == '/') {
-				do { ++p; } while (*p == '/');
-				c  = *p;
-				*p = '\0';
+	int curdir = open_read(".");
+	
+	if (curdir == -1)
+		return -1;
+	
+	if (!strtok_init_str(st, path, "/", 0))
+		return -1;
+	
+	strtok_for_each(st, p) {
+		if (mkdir(p->token, 0777) == -1) {
+			if (errno != EEXIST || stat(p->token, &sb) == -1) {
+				ok = 0;
 				break;
 			}
 			
-			else
-				++p;
-		}
-		
-		if (mkdir(buf, 0777) == -1) {
-			if (errno != EEXIST || stat(buf, &sb) == -1)
-				goto err;
-			
 			if (!S_ISDIR(sb.st_mode)) {
 				errno = ENOTDIR;
-				goto err;
+				ok = 0;
+				break;
 			}
-			
-			if (!c)
-				goto out;
 		}
 		
-		if (!c) {
-			if (chmod(buf, mode) == -1)
-				goto err;
-			
-			goto out;
+		if (chdir(p->token) == -1) {
+			ok = 0;
+			break;
 		}
-		
-		*p = c;
-	} while (1);
+	}
 	
-err:
-	rc = -1;
-out:
-	errno_orig = errno;
-	free(buf);
-	errno = errno_orig;
-	return rc;
+	if (ok && chmod(".", mode) == -1)
+		return -1;
+	
+	fchdir(curdir);
+	close(curdir);
+	
+	strtok_free(st);
+	return ok ? 0 : -1;
 }
