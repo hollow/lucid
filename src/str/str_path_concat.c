3008ed1312c0ec72a791124515e8049f6d9e1eb1
Benedikt Böhm <bb@xnull.de>
2007-01-29 13:45:30 +0000

    concat paths if relative to ./

--- src/str/str_path_concat.c
+++ src/str/str_path_concat.c
@@ -15,17 +15,21 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
 
+#include "mem.h"
 #include "printf.h"
 #include "str.h"
 
 char *str_path_concat(const char *dirname, const char *basename)
 {
 	char *path = 0;
-	
+
+	if (str_len(basename) > 1 && mem_cmp(basename, "./", 2) == 0)
+		basename += 2;
+
 	if (str_isempty(dirname) || str_path_isdot(basename))
 		return 0;
-	
+
 	_lucid_asprintf(&path, "%s/%s", dirname, basename);
-	
+
 	return path;
 }
